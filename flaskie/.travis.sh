82cdf8d9a91ded38aa44bc5877c94f6cb27cbeab
asheuh <asheuh49@gmail.com>
2018-07-16 12:52:40 +0300

    add travis database

---
+++ flaskie/.travis.sh
@@ -0,0 +1,6 @@
+export DATABASE_NAME='flaskie_test'
+export DATABASE_USER='asheuh'
+export DATABASE_PASSWORD='mermaid'
+export DATABASE_HOST='localhost'
+export SECRET_KEY='i love hot ladies'
+export ENVIRONMENT="DEVELOPMENT"
